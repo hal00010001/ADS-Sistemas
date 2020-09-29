7cfdc98192294212e9aed4316af69af9a6028b2b
rafaelmpereira <51132459+rafaelmpereira@users.noreply.github.com>
2020-09-28 21:26:34 -0300

    Update AdicionarCliente.js

--- agr-bikes-react/src/api/endpoints/AdicionarCliente.js
+++ agr-bikes-react/src/api/endpoints/AdicionarCliente.js
@@ -32,33 +32,42 @@ const AdicionarCliente = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="text" 
-                placeholder="Nome" 
-                name="" value={nome} 
-                onChange={({ target }) => setNome(target.value)} 
-            />
-            <input 
-                type="text" 
-                placeholder="CPF" 
-                name="" value={cpf} 
-                onChange={({ target }) => setCpf(target.value)} 
-            />
-            <input 
-                type="email" 
-                placeholder="Email" 
-                name="" value={email} 
-                onChange={({ target }) => setEmail(target.value)} 
-            />
-            <input 
-                type="text" 
-                placeholder="Telefone" 
-                name="" value={telefone} 
-                onChange={({ target }) => setTelefone(target.value)} 
-            />
-            <button type="submit">Enviar</button>
-        </form>
+	  <div className="row">
+		<div className="col-xl-12 col-lg-7">
+		  <div className="card shadow mb-4">
+			<div className="card-header py-3">
+			  <h6 className="m-0 font-weight-bold text-primary"><i className="fas fa-fw fa-user-edit"></i> Adicionar Clientes</h6>
+			</div>
+			<div className="card-body">
+			  <form onSubmit={handleSubmit} className="user">
+      			<div className="form-group">
+				  <div className="row">
+				  <div className="col-sm-6 mb-3 mb-sm-4">
+					<input type="text" className="form-control form-control-user" id="" placeholder="Nome" value={nome} onChange={({ target }) => setNome(target.value)}
+					/>
+				  </div>
+				  <div className="col-sm-6 mb-3 mb-sm-4">
+					<input type="text" className="form-control form-control-user" id="" placeholder="CPF" value={cpf} onChange={({ target }) => setCpf(target.value)}
+					/>
+				  </div>
+				  </div>
+				  <div className="row">
+				  <div className="col-sm-6 mb-3 mb-sm-4">
+					<input type="email" className="form-control form-control-user" id="" placeholder="E-mail" value={email} onChange={({ target }) => setEmail(target.value)}
+					/>
+				  </div>
+				  <div className="col-sm-6 mb-3 mb-sm-4">
+					<input type="text" className="form-control form-control-user" id="" placeholder="Telefone" value={telefone} onChange={({ target }) => setTelefone(target.value)}
+					/>
+				  </div>
+				  </div>
+  		  	      <button type="submit" className="btn btn-primary btn-user col-sm-12 mb-3 mb-sm-4"> Cadastrar Cliente </button>
+				</div>
+			  </form>
+			</div>
+		  </div>
+		</div>
+	  </div>
     );
 };
 
